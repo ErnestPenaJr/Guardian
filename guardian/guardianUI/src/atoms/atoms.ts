56becc370fbaac45f0820e05d1860b4df07d60a0
Jonathan Higdon <jhigdon@gmail.com>
2025-01-20 11:37:42 -0500

    adding userInfo atom defaulting user for now

--- guardian/guardianUI/src/atoms/atoms.ts
+++ guardian/guardianUI/src/atoms/atoms.ts
@@ -14,3 +14,18 @@ export function atomWithToggle(
 export const isOpenAtom = atomWithToggle(false)
 
 export const sidebarAtom = atom(true)
+
+
+let currentUser1 = {
+  role: 'Administrator',
+  email: 'testadmin@shieldlytics.com',
+  status: 'Active'
+}
+
+export type UserInfo = {
+  role: string
+  email: string
+  status: string
+}
+
+export const userInfoAtom = atom<UserInfo>(currentUser1);
