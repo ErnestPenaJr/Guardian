5cd638c97e91bd2a9e05957a9c9be5493814f347
Jonathan Higdon <jhigdon@gmail.com>
2025-02-05 18:03:34 -0500

    add fetch for users

--- guardian/guardianUI/src/services/adminServices.ts
+++ guardian/guardianUI/src/services/adminServices.ts
@@ -7,8 +7,21 @@ export const inviteUser = async (email: string) => {
         ...jsonHeader,
         body: JSON.stringify({ email })
     });
-    
+
     return response.json();
 }
+export type UserGridData = {
+    id: number;
+    lastName: string;
+    firstName: string;
+    email: string;
+    role: string;
+    userStatus: string;
+};
 
 
+export const fetchUsers = () : Promise<UserGridData[]> => 
+    fetch(`${apiRoot}/users`,{
+        method: 'GET',
+        ...jsonHeader
+    }).then(resp => handleResponse(resp));
