5ed034909f8ab99863455cebd4f79f5b297d1fd6
Jonathan Higdon <jhigdon@gmail.com>
2025-02-22 11:03:23 -0500

    some documment entity updates

---
+++ guardian/guardianAPI/database/guardian/entities/RequestAttachment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Database.Guardian.Entities;
+
+public partial class RequestAttachment
+{
+    public Guid Id { get; set; }
+
+    public Guid RequestId { get; set; }
+
+    public Guid DocumentId { get; set; }
+
+    public string FileName { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public Guid CreatedBy { get; set; }
+
+    public byte[] DataVersion { get; set; } = null!;
+}
--- guardian/guardianAPI/repositories/DocumentRepository.cs
+++ guardian/guardianAPI/repositories/DocumentRepository.cs
@@ -1,12 +1,39 @@
+using Database.Guardian.Entities;
 using Models;
 
 namespace Repositories
 {
-    public class DocumentRepository
+    public class DocumentRepository(GuardianDbContext db, ILogger<DocumentRepository> logger)
     {
-        public void Save(DocumentData document)
+        public async Task<(DocumentInfo? DocumentInfo, bool Success)> Save(IFormFile file, Guid UserId)
         {
             // Save document to database
+            try {
+                using var ms = new MemoryStream();
+                await file.CopyToAsync(ms);
+
+                var doc = await db.Documents.AddAsync(new Document {
+                    FileName = file.FileName,
+                    FileType = file.ContentType,
+                    FileData = ms.ToArray(),
+                    CreatedBy = UserId,
+                    CreatedAt = DateTime.UtcNow,
+                });
+
+                var changes = await db.SaveChangesAsync();
+
+                return changes > 0
+                    ? (new DocumentInfo {
+                        Id = doc.Entity.Id,
+                        FileName = doc.Entity.FileName,
+                        FileType = doc.Entity.FileType,
+                    }, true)
+                    : (null, false);
+
+            } catch(Exception e) {
+                logger.LogError("error saving document for user {UserId}: {e}, {e.Message}", UserId, e, e.Message);
+                return (null, false);
+            }
         }
 
         public void Delete(int documentId)
