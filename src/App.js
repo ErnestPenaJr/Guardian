cef4c01eabe2119dac109c1756cb9deab098a774
Joel Upright <Joel@shieldlytics.com>
2025-03-12 19:42:04 +0000

    Added 4 files to /src

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GuardianLogin from './GuardianLogin';
+
+function App() {
+  return (
+    <div>
+      <GuardianLogin />
+    </div>
+  );
+}
+
+export default App;
